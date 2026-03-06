57c4f1abaa0c3d964edd4e81456f8c05f2fd8870
sturtz <sturtzmorgan@gmail.com>
2026-03-06 07:33:33 -0500

    Revert "stop tracking node_modules"
    This reverts commit 064799b350eaaef235c1a392919dc33e3f5dcdde.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "./",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
